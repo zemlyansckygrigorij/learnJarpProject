ad94a7e65a4d2784d7ed9705b9cde7914a71dd18
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-21 14:32:19 +0500

    update class Student create variables and methods

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,6 +1,109 @@
 package com.example.demo.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Student {
     private int id;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private Date dateBirthday;
+    private String address;
+    private boolean marriage;
+    private Gender gender;
 
+    public Student() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && marriage == student.marriage && Objects.equals(firstName, student.firstName) && Objects.equals(secondName, student.secondName) && Objects.equals(lastName, student.lastName) && Objects.equals(dateBirthday, student.dateBirthday) && Objects.equals(address, student.address) && gender == student.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, secondName, lastName, dateBirthday, address, marriage, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateBirthday=" + dateBirthday +
+                ", address='" + address + '\'' +
+                ", marriage=" + marriage +
+                ", gender=" + gender +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateBirthday() {
+        return dateBirthday;
+    }
+
+    public void setDateBirthday(Date dateBirthday) {
+        this.dateBirthday = dateBirthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isMarriage() {
+        return marriage;
+    }
+
+    public void setMarriage(boolean marriage) {
+        this.marriage = marriage;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
