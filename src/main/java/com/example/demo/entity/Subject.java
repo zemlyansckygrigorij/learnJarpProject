0a2d883f75cd6e1b1cf3682e77ed215252b6c5d3
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-21 14:24:18 +0500

    update class Subject create variables and methods

--- src/main/java/com/example/demo/entity/Subject.java
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -1,4 +1,58 @@
 package com.example.demo.entity;
 
+import java.util.Objects;
+
 public class Subject {
+    private int id;
+    private String name;
+    private String description;
+
+    public Subject() {
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return id == subject.id && Objects.equals(name, subject.name) && Objects.equals(description, subject.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
