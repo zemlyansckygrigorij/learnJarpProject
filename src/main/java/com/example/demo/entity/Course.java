a0c962ecaf4ea9c65f8a722ac45c089a10fb8de2
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-21 14:19:07 +0500

    update class Course create variables and methods

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,4 +1,65 @@
 package com.example.demo.entity;
 
+import java.util.Objects;
+
 public class Course {
+    private int id;
+    private String name;
+    private double cost;
+    private int duration;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && Double.compare(course.cost, cost) == 0 && duration == course.duration && Objects.equals(name, course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, cost, duration);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cost=" + cost +
+                ", duration=" + duration +
+                '}';
+    }
 }
