e69a3a8c1f551a31b241d10085ae3e30c0610660
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-21 14:38:25 +0500

    create enum Gender

---
+++ src/main/java/com/example/demo/entity/Gender.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+public enum Gender {
+    MALE  {
+        public String toString() {
+            return "male";
+        }
+    }
+
+        ,FEMALE  {
+        public String toString() {
+            return "female";
+        }
+    }
+}
