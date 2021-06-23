fcf1f72966656d71196b27b321fd5f676e820ed5
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 12:25:38 +0500

    update  class NGTest create method public static void main

--- src/test/java/com/example/demo/NGTest.java
+++ src/test/java/com/example/demo/NGTest.java
@@ -1,4 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.entity.CourseTestNG;
+
 public class NGTest {
+    public static void main(String[] args) throws Exception {
+        CourseTestNG courseTestNG = new CourseTestNG();
+        courseTestNG.makeTest();
+    }
 }
