c42c7f3276ca574ddec48d343b6ccc04af8842dc
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:33:53 +0500

    update class TeacherRepositoryTest for testing database

--- src/test/java/com/example/demo/repository/TeacherRepositoryTest.java
+++ src/test/java/com/example/demo/repository/TeacherRepositoryTest.java
@@ -1,7 +1,40 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Subject;
+import com.example.demo.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TeacherRepositoryTest {
+    @Autowired
+    private TeacherRepository repository;
+    @Test
+    public void makeTest(){
+        long countFirst = repository.count();
+        assertEquals(0, countFirst);
 
+        Teacher teacher = new Teacher();
+        teacher.setAddress("tenneccy");
+        teacher.setFirstName("Alfred");
+        teacher.setSecondName("derec");
+        teacher.setLastName("maison");
+        teacher.setBiography("description");
+        teacher.setDateBirth(new Date());
+        teacher.setQualification("high");
+        repository.save(teacher);
+
+        long countSecond = repository.count();
+        assertEquals(1, countSecond);
+    }
 }
