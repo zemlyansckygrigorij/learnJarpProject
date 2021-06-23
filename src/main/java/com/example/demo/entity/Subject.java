4872bba92227a8a9688e033e21005f5ea2e45652
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:24:30 +0500

    update class Subject add annotation @Table(name = "subjects")

--- src/main/java/com/example/demo/entity/Subject.java
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -1,12 +1,10 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "subjects")
 public class Subject {
 
     @Id
