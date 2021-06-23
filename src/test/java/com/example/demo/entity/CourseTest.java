d897b01866f084dc2877f24b877e69f23a2c86e8
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 12:22:06 +0500

    update  class CourseTest make test method makeTest()

--- src/test/java/com/example/demo/entity/CourseTest.java
+++ src/test/java/com/example/demo/entity/CourseTest.java
@@ -1,7 +1,34 @@
 package com.example.demo.entity;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
+    @Test
+    public void makeTest()  {
+        Course course = new Course();
+        course.setId(1);
+        course.setCost(2.3);
+        course.setDuration(12);
+        course.setName("Math");
+        assertEquals(course.getId(),1);
+        assertEquals(course.getCost(),2.3);
+        assertEquals(course.getName(),"Math");
+        assertEquals(course.getDuration(),12);
 
+
+
+        assertNotEquals(course.getId(),12);
+        assertNotEquals(course.getCost(),2.1);
+        assertNotEquals(course.getName(),"Math2");
+        assertNotEquals(course.getDuration(),121);
+
+        CourseTestNG courseTestNG = new CourseTestNG();
+        try {
+            courseTestNG.makeTest();
+        }catch(Exception e){
+            e.getStackTrace();
+        }
+    }
 }
