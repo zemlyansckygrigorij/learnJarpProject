1c50a37e0d7752ef8224657640e2bcd466e7634f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 13:38:09 +0500

    update class Teacher change  @GeneratedValue from GenerationType.AUTO to GenerationType.SEQUENCE

--- src/main/java/com/example/demo/entity/Teacher.java
+++ src/main/java/com/example/demo/entity/Teacher.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class Teacher {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
     private String firstName;
