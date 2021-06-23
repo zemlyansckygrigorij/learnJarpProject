84de63330f4194e46c8bfb9c94e952dd58546300
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:27:01 +0500

    update class SubjectRepositoryTest for testing database

--- src/test/java/com/example/demo/repository/SubjectRepositoryTest.java
+++ src/test/java/com/example/demo/repository/SubjectRepositoryTest.java
@@ -1,7 +1,35 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Course;
+import com.example.demo.entity.Subject;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class SubjectRepositoryTest {
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SubjectRepositoryTest {
+    @Autowired
+    private SubjectRepository repository;
+    @Test
+    public void makeTest(){
+        long countFirst = repository.count();
+        assertEquals(0, countFirst);
+
+        Subject subject = new Subject();
+        subject.setName("Math");
+        subject.setDescription("Mathematics");
+
+        repository.save(subject);
+
+        long countSecond = repository.count();
+        assertEquals(1, countSecond);
+    }
 }
