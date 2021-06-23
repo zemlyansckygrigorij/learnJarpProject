60e8c6ab9804a92b098991ef3182f4a35cb65b02
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:12:50 +0500

    update class StudentRepositoryTest create test method makeTest()

--- src/test/java/com/example/demo/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/repository/StudentRepositoryTest.java
@@ -1,7 +1,41 @@
 package com.example.demo.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.demo.entity.Gender;
+import com.example.demo.entity.Student;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Date;
+
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository repository;
+    @Test
+    public void makeTest(){
+        long countFirst = repository.count();
+        Assert.assertEquals("beforeInsert",1, countFirst);
 
+        Student student = new Student();
+        student.setAddress("tehas");
+        student.setDateBirthday(new Date());
+        student.setFirstName("jheck");
+        student.setSecondName("maison");
+        student.setLastName("stehan");
+        student.setGender(Gender.man);
+        student.setMarriage(false);
+        repository.save(student);
+
+        long countSecond = repository.count();
+        Assert.assertEquals("afterInsert",2, countSecond);
+    }
 }
