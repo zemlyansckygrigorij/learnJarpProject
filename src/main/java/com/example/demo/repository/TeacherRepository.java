5e1d7af81647c6fc1d2a387ca7d5cd41a9621fd0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:26:30 +0500

    update interface TeacherRepository add annotation @Repository

--- src/main/java/com/example/demo/repository/TeacherRepository.java
+++ src/main/java/com/example/demo/repository/TeacherRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeacherRepository  extends JpaRepository<Teacher,Integer> {
 
 }
