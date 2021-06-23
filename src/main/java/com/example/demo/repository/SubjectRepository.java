e2edce942b93212e8e010b9f7600f38ac7c4d84d
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 16:25:56 +0500

    update interface SubjectRepository add annotation @Repository

--- src/main/java/com/example/demo/repository/SubjectRepository.java
+++ src/main/java/com/example/demo/repository/SubjectRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectRepository  extends JpaRepository<Subject,Integer> {
 
 }
