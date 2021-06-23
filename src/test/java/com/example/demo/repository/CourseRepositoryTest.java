b77a78f4b0e7c039254d6f7273651e2282892c56
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 13:34:25 +0500

    update class CourseRepositoryTest  create  test method

--- src/test/java/com/example/demo/repository/CourseRepositoryTest.java
+++ src/test/java/com/example/demo/repository/CourseRepositoryTest.java
@@ -1,7 +1,33 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.jupiter.api.Assertions.*;
 
-class CourseRepositoryTest {
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CourseRepositoryTest {
+    @Autowired
+    private CourseRepository repository;
+    @Test
+    public void makeTest(){
+        long countFirst = repository.count();
+        assertEquals(0, countFirst);
+
+        Course course = new Course();
+        course.setName("math");
+        course.setCost(123);
+        course.setDuration(12);
+        repository.save(course);
+
+        long countSecond = repository.count();
+        assertEquals(1, countSecond);
+    }
 }
