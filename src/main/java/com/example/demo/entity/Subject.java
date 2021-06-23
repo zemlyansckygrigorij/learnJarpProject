a4f64ad97e2eb9955084c40f9e57bef31b3b3d33
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 12:20:58 +0500

    update  class Subject add Entity

--- src/main/java/com/example/demo/entity/Subject.java
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -1,9 +1,18 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
     private String name;
     private String description;
 
