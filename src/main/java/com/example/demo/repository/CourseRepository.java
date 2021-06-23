fc125658e06613ebc42be0f5ce659ccaa9b62988
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 12:42:30 +0500

    update interface CourseRepository add @Repository and method getCourseByName

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.repository;
 
-public interface CourseRepository {
+import com.example.demo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Integer> {
+    @Query("SELECT e FROM Course e WHERE e.name = :name")
+    public Optional getCourseByName(@Param("name") String name);
 }
