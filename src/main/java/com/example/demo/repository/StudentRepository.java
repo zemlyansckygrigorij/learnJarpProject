ff0992438d832d1bd68c62f82c89ac05e0690cb0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 13:42:41 +0500

    update interface StudentRepository  add extends JpaRepository<Student,Integer>

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repository;
 
-public interface StudentRepository {
+import com.example.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository  extends JpaRepository<Student,Integer> {
 
 }
