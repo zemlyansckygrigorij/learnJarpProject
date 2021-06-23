19ae7e24d5480e96fa2155014b0c517b571b9f05
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-23 12:21:13 +0500

    update  class Teacher add Entity

--- src/main/java/com/example/demo/entity/Teacher.java
+++ src/main/java/com/example/demo/entity/Teacher.java
@@ -1,10 +1,19 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
     private String firstName;
     private String secondName;
     private String lastName;
