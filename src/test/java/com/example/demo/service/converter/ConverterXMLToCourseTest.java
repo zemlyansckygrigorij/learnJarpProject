9dd965e4eac0eb6ac76e44d7fee5283f1f96c8c7
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-06-24 11:38:41 +0500

    update class ConverterXMLToCourseTest for testing ConverterXMLToCourse

--- src/test/java/com/example/demo/service/converter/ConverterXMLToCourseTest.java
+++ src/test/java/com/example/demo/service/converter/ConverterXMLToCourseTest.java
@@ -1,7 +1,27 @@
 package com.example.demo.service.converter;
 
+import com.example.demo.entity.Course;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ConverterXMLToCourseTest {
 
+
+@SpringBootTest
+class ConverterXMLToCourseTest {
+    @Value("${spring.dir.courses}")
+    private String parth;
+
+
+    @Test
+    public void makeTest(){
+
+        ConverterXMLToCourse converter = new ConverterXMLToCourse(parth);
+        List<Course> courses = converter.getListCourses() ;
+        assertEquals(4,courses.size());
+    }
 }
